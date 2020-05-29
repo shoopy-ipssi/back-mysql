27f7d0911a5bc408fe5d29b7b8b34c0022131b59
GenoudJeremy <43404723+GenoudJeremy@users.noreply.github.com>
2020-05-29 11:34:59 +0200

    Update saves.js

--- app/routes/saves.js
+++ app/routes/saves.js
@@ -5,7 +5,7 @@ module.exports = function (app, db) {
 
     // get all saves
     app.get('/saves', (req, res) => {
-        let sql = `SELECT * FROM saves WHERE id_user = '${req.body.iduser}'`;
+        let sql = `SELECT * FROM saves WHERE id_user = '${req.params.iduser}'`;
         db.query(sql, (err, result) => {
             if(err) throw err;
             console.log(result);
